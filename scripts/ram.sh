883d66dbf52595f1b3da38852e355e7cbf431213
WhiteBlackGoose <wbg@angouri.org>
2022-08-24 12:21:32 +0300

    fixed ram usage

--- scripts/ram.sh
+++ scripts/ram.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+
+# https://github.com/KittyKatt/screenFetch/issues/386#issuecomment-249312716
+
 total=$(expr $(cat /proc/meminfo | grep MemTotal | head -n 1 | awk '{ print $2 }') / 1024)
-used=$(expr $(cat /proc/meminfo | grep MemAvailable | head -n 1 | awk '{ print $2 }') / 1024)
-used=$(expr $total - $used)
+free=$(expr $(cat /proc/meminfo | grep MemFree | head -n 1 | awk '{ print $2 }') / 1024)
+shmem=$(expr $(cat /proc/meminfo | grep Shmem | head -n 1 | awk '{ print $2 }') / 1024)
+buffers=$(expr $(cat /proc/meminfo | grep Buffers | head -n 1 | awk '{ print $2 }') / 1024)
+cached=$(expr $(cat /proc/meminfo | grep Cached | head -n 1 | awk '{ print $2 }') / 1024)
+reclaimable=$(expr $(cat /proc/meminfo | grep SReclaimable | head -n 1 | awk '{ print $2 }') / 1024)
+
+used=$(expr $total + $shmem - $free - $buffers - $cached - $reclaimable)
+
 
 percent=$(echo "scale=1; 100*$used/$total" | bc)
 printf "   %s %% / %'d Mb " $percent $used
