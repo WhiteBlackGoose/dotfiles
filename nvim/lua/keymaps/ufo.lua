f692dd548f3bf57df21a58771559770c18fde050
WhiteBlackGoose <wbg@angouri.org>
2022-11-21 21:22:06 +0300

    minor change

--- nvim/lua/keymaps/ufo.lua
+++ nvim/lua/keymaps/ufo.lua
@@ -1,4 +1,4 @@
 add('n', '<leader>j', 'zc')
-add('n', '<leader>k', 'zo')
+add('n', '<leader>k', 'zo:IndentBlanklineRefresh<CR>')
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
