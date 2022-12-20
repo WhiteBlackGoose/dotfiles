cec87fae5e89d9482a9bd81ad6a67ed88df6c059
WhiteBlackGoose <wbg@angouri.org>
2022-12-20 10:23:26 +0300

    windows keybinds

---
+++ nvim/lua/keymaps/windows.lua
@@ -0,0 +1,22 @@
+-- Resize
+res = '4'
+add('n', 'WW', '<C-w>4+')
+add('n', 'WS', '<C-w>4-')
+add('n', 'WD', '<C-w>4>')
+add('n', 'WA', '<C-w>4<')
+
+-- Split
+add('n', 'Wv', '<C-w>v')
+add('n', 'Ws', '<C-w>s')
+
+-- Move among windows
+add('n', 'Wh', '<C-w>h')
+add('n', 'Wj', '<C-w>j')
+add('n', 'Wk', '<C-w>k')
+add('n', 'Wl', '<C-w>l')
+
+-- Swap windows
+add('n', 'WH', '<C-w>H')
+add('n', 'WJ', '<C-w>J')
+add('n', 'WK', '<C-w>K')
+add('n', 'WL', '<C-w>L')
--- nvim/lua/settings/template.lua
+++ nvim/lua/settings/template.lua
@@ -1,5 +1,3 @@
 local temp = require('template')
 
 temp.temp_dir = '/home/goose/.config/nvim/templates'
-
-require("telescope").load_extension('find_template')
