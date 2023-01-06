b7023d170bd387ae72e73ecc2b9db29c710d6d31
WhiteBlackGoose <wbg@angouri.org>
2023-01-06 19:25:11 +0300

    upd ltex

--- nvim/lua/settings/latex.lua
+++ nvim/lua/settings/latex.lua
@@ -1,12 +1,24 @@
 require'lspconfig'.texlab.setup{}
-vim.g.vimtex_compiler_latexmk = {
-    build_dir = '.out',
-    options = {
-        '-shell-escape',
-        '-verbose',
-        '-file-line-error',
-        '-interaction=nonstopmode',
-        '-synctex=1'
+function TexMoveOut(path)
+    vim.g.vimtex_compiler_latexmk = {
+        build_dir = path,
+        options = {
+            '-shell-escape',
+            '-verbose',
+            '-file-line-error',
+            '-interaction=nonstopmode',
+            '-synctex=1'
+        }
     }
-}
+end
+
+function TexMoveOutAbove()
+    TexMoveOut('../.out')
+    vim.cmd[[:VimtexReload]]
+end
+
+TexMoveOut('.out')
 vim.g['vimtex_view_method'] = 'zathura'
+vim.cmd[[
+:command TexMoveOutAbove lua TexMoveOutAbove()<CR>
+]]
