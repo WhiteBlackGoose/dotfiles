5ba10d06d39dd3684d272123a2d99f84f26e138a
WhiteBlackGoose <wbg@angouri.org>
2023-02-14 10:00:51 +0300

    fetcher of tools in another file

--- envs/dotnet.nix
+++ envs/dotnet.nix
@@ -24,38 +24,12 @@ pkgs.mkShell rec {
     msbuild
   ] ++ deps;
 
-
-  tools = let 
-    dotnet-tool = name: dllname: version: arch: sha256:
-      let 
-        drv =
-          pkgs.fetchzip {
-            url = "https://www.nuget.org/api/v2/package/${name}/${version}";
-            sha256 = sha256;
-            extension = "zip";
-            stripRoot = false;
-          };
-        fake = pkgs.writeScript "${name}-fake" 
-          "dotnet ${drv}/tools/${arch}/${dllname}.dll";
-      in
-        pkgs.runCommand name {} ''
-          mkdir $out
-          ln -s ${fake} $out/${name}
-        '';
-    in {
-    fsautocomplete = dotnet-tool "fsautocomplete" "fsautocomplete" "0.58.4" "net7.0/any"
-      "sha256-PLO24n2zcRPWR6/ihAnCT2Y8kBH9WCHhlkOqc7xEujg=";
-
-    csharp-ls = dotnet-tool "csharp-ls" "CSharpLanguageServer" "0.6.0" "net7.0/any"
-      "sha256-z3rbrQ4YUE3Etm7S0oku4PPsXWXhu06+nofXW+uJTsk=";
-
-    __toString = x: "";
-  };
-
-
-  shellHook = ''
+  shellHook = let
+    dotnet-tool = (import ./dotnet-tool.nix { pkgs = pkgs; dotnet = dotnetPkg; });
+  in
+  ''
     DOTNET_ROOT="${dotnetPkg}";
-    PATH="${tools.fsautocomplete}:${tools.csharp-ls}:~/.dotnet/tools:$PATH";
+    PATH="${dotnet-tool.fsautocomplete}:${dotnet-tool.csharp-ls}:~/.dotnet/tools:$PATH";
   '';
 }
 
