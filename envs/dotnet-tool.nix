e219c40a74b5bdee91e4f2ee38cc5005fc4cee05
WhiteBlackGoose <wbg@angouri.org>
2023-02-14 11:07:43 +0300

    making dotnet-tool a derivation... WIP

--- envs/dotnet-tool.nix
+++ envs/dotnet-tool.nix
@@ -1,34 +1,41 @@
-{ pkgs, dotnet }:
+{ pkgs, stdenv  }:
 
 rec {
-  fetch = 
-    { name, dllname, version, arch, sha256 }:
+  combineTools = dotnet: list:
     let 
-      drv =
-        pkgs.fetchzip {
+      drv = { name, dllname, version, arch, sha256 }:
+        let d = pkgs.fetchzip {
           url = "https://www.nuget.org/api/v2/package/${name}/${version}";
           sha256 = sha256;
           extension = "zip";
           stripRoot = false;
         };
-      fake = pkgs.writeScript "${name}-fake" 
-        "DOTNET_ROOT=${dotnet} ${dotnet}/dotnet ${drv}/tools/${arch}/${dllname}.dll";
+        in
+        pkgs.writeScript "${name}-fake" 
+          "DOTNET_ROOT=${dotnet} ${dotnet}/dotnet ${d}/tools/${arch}/${dllname}.dll";
     in
-    pkgs.runCommand name {} ''
-      mkdir $out
-      ln -s ${fake} $out/${name}
-    '';
+    pkgs.runCommand "tools" {} (''
+      mkdir -p $out/bin
+    '' +
+      (builtins.concatStringsSep "\n" 
+        (builtins.map 
+          (pkg: ''ln -s ${drv pkg} $out/bin/${pkg.name}'')
+          list
+        )
+      ));
 
-  fsautocomplete = fetch { 
-    name = "fsautocomplete"; dllname = "fsautocomplete"; 
-    version = "0.58.4"; arch = "net7.0/any";
-    sha256 = "sha256-PLO24n2zcRPWR6/ihAnCT2Y8kBH9WCHhlkOqc7xEujg=";
-  };
+  tools = {
+    fsautocomplete = { 
+      name = "fsautocomplete"; dllname = "fsautocomplete"; 
+      version = "0.58.4"; arch = "net7.0/any";
+      sha256 = "sha256-PLO24n2zcRPWR6/ihAnCT2Y8kBH9WCHhlkOqc7xEujg=";
+    };
 
-  csharp-ls = fetch {
-    name = "csharp-ls"; dllname = "CSharpLanguageServer";
-    version = "0.6.0"; arch = "net7.0/any";
-    sha256 = "sha256-z3rbrQ4YUE3Etm7S0oku4PPsXWXhu06+nofXW+uJTsk=";
+    csharp-ls = {
+      name = "csharp-ls"; dllname = "CSharpLanguageServer";
+      version = "0.6.0"; arch = "net7.0/any";
+      sha256 = "sha256-z3rbrQ4YUE3Etm7S0oku4PPsXWXhu06+nofXW+uJTsk=";
+    };
   };
 }
 
--- envs/dotnet.nix
+++ envs/dotnet.nix
@@ -11,25 +11,33 @@ pkgs.mkShell rec {
     zlib
     zlib.dev
     openssl
-    dotnetPkg
+    
+    (
+      let 
+        dt = (callPackage ./dotnet-tool.nix {});
+      in
+        dt.combineTools dotnetPkg 
+          (with dt.tools; [
+            fsautocomplete
+            csharp-ls
+          ])
+    )
   ];
 
   NIX_LD_LIBRARY_PATH = lib.makeLibraryPath ([
     stdenv.cc.cc
   ] ++ deps);
   NIX_LD = "${pkgs.stdenv.cc.libc_bin}/bin/ld.so";
-  nativeBuildInputs = [ 
+  packages = [ 
     omnisharp-roslyn
     netcoredbg
     msbuild
   ] ++ deps;
 
-  shellHook = let
-    dotnet-tool = (import ./dotnet-tool.nix { pkgs = pkgs; dotnet = dotnetPkg; });
-  in
+  shellHook = 
   ''
     DOTNET_ROOT="${dotnetPkg}";
-    PATH="${dotnet-tool.fsautocomplete}:${dotnet-tool.csharp-ls}:~/.dotnet/tools:$PATH";
+    PATH="~/.dotnet/tools:$PATH";
   '';
 }
 
