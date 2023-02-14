5bc23ba9ead69e26296a4efc33f33c4dd84e853d
WhiteBlackGoose <wbg@angouri.org>
2023-02-14 09:30:34 +0300

    dotnet tools are fetched with a function now

--- envs/dotnet.nix
+++ envs/dotnet.nix
@@ -24,25 +24,38 @@ pkgs.mkShell rec {
     msbuild
   ] ++ deps;
 
-  # Making F#'s fsautocomplete work:
-  fsautocomplete-drv =
-    pkgs.fetchzip {
-      url = "https://www.nuget.org/api/v2/package/fsautocomplete/0.58.4";
-      sha256 = "sha256-PLO24n2zcRPWR6/ihAnCT2Y8kBH9WCHhlkOqc7xEujg=";
-      extension = "zip";
-      stripRoot = false;
-    };
 
-  fsautocomplete-fake = pkgs.writeScript "fsautocomplete-fake" "dotnet ${fsautocomplete-drv}/tools/net7.0/any/fsautocomplete.dll";
+  tools = let 
+    dotnet-tool = name: dllname: version: arch: sha256:
+      let 
+        drv =
+          pkgs.fetchzip {
+            url = "https://www.nuget.org/api/v2/package/${name}/${version}";
+            sha256 = sha256;
+            extension = "zip";
+            stripRoot = false;
+          };
+        fake = pkgs.writeScript "${name}-fake" 
+          "dotnet ${drv}/tools/${arch}/${dllname}.dll";
+      in
+        pkgs.runCommand name {} ''
+          mkdir $out
+          ln -s ${fake} $out/${name}
+        '';
+    in {
+    fsautocomplete = dotnet-tool "fsautocomplete" "fsautocomplete" "0.58.4" "net7.0/any"
+      "sha256-PLO24n2zcRPWR6/ihAnCT2Y8kBH9WCHhlkOqc7xEujg=";
 
-  fsautocomplete = pkgs.runCommand "fsautocomplete" {} ''
-    mkdir $out
-    ln -s ${fsautocomplete-fake} $out/fsautocomplete
-  '';
+    csharp-ls = dotnet-tool "csharp-ls" "CSharpLanguageServer" "0.6.0" "net7.0/any"
+      "sha256-z3rbrQ4YUE3Etm7S0oku4PPsXWXhu06+nofXW+uJTsk=";
+
+    __toString = x: "";
+  };
+
 
   shellHook = ''
     DOTNET_ROOT="${dotnetPkg}";
-    PATH="${fsautocomplete}:~/.dotnet/tools:$PATH";
+    PATH="${tools.fsautocomplete}:${tools.csharp-ls}:~/.dotnet/tools:$PATH";
   '';
 }
 
