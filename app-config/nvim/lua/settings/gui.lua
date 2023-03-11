fe0345f7c51892ef35b05f8d1a0a7a1c9dd3122f
WhiteBlackGoose <wbg@angouri.org>
2023-03-11 11:42:16 +0600

    gui

---
+++ app-config/nvim/lua/settings/gui.lua
@@ -0,0 +1,8 @@
+-- vim.o.guifont = "FuraMono Nerd Font Mono:h9"
+vim.o.guifont = "FiraCode Nerd Font:h9"
+vim.cmd[[set mouse=nv]]
+vim.g.neovide_cursor_vfx_mode = "sonicboom"
+vim.g.neovide_refresh_rate = 90
+vim.g.neovide_input_use_logo = false
+vim.g.neovide_scroll_animation_length = 0.3
+vim.g.neovide_cursor_animate_in_insert_mode = true
